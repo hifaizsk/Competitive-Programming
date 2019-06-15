088a052804e396ad4d7adbbc49448433a149c4c6
Faiz Shaikh <hifaizsk@gmail.com>
2019-06-15 23:33:36 +0530

    LC: Solved best time to buy and sell stock

--- LeetCode/Best_Time_To_buy_and_sell_stock.java
+++ LeetCode/Best_Time_To_buy_and_sell_stock.java
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/best-time-to-buy-and-sell-stock
 
 class Solution {
-  public int maxProfit(int[] prices) {
+  public static int maxProfit(int[] prices) {
     int maxProfit = 0;
     int minProfit = Integer.MAX_VALUE;
 
@@ -13,7 +13,7 @@ class Solution {
   }
 
   public static void main(String[] args) {
-    System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
-    System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    System.out.println(maxProfit(new int[] { 7, 1, 5, 3, 6, 4 }));
+    System.out.println(maxProfit(new int[] { 7, 6, 4, 3, 1 }));
   }
 }
